d8fb3fc2182c68d8b98f2c6e39736d91a5615fda
carlosribeiro <carlos.a.o.r@icloud.com>
2026-04-09 18:39:42 -0500

    chore: setup Clean Architecture folder structure

--- app/src/main/java/com/carlosribeiro/reelcine/MainActivity.kt
+++ app/src/main/java/com/carlosribeiro/reelcine/MainActivity.kt
@@ -11,7 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.carlosribeiro.reelcine.ui.theme.ReelCineTheme
+import com.carlosribeiro.reelcine.presentation.theme.ReelCineTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
