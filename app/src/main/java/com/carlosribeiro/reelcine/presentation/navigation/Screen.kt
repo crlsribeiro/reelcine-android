1ef432eec2f664684d9a2deee39edf318c043ffd
carlosribeiro <carlos.a.o.r@icloud.com>
2026-04-11 09:11:55 -0500

    feat: add movie list screen with grid layout and see all navigation

--- app/src/main/java/com/carlosribeiro/reelcine/presentation/navigation/Screen.kt
+++ app/src/main/java/com/carlosribeiro/reelcine/presentation/navigation/Screen.kt
@@ -9,6 +9,9 @@ sealed class Screen(val route: String) {
     data object MovieDetail : Screen("movie_detail/{movieId}") {
         fun createRoute(movieId: Int) = "movie_detail/$movieId"
     }
+    data object MovieList : Screen("movie_list/{category}") {
+        fun createRoute(category: String) = "movie_list/$category"
+    }
     data object Groups : Screen("groups")
     data object GroupDetail : Screen("group_detail/{groupId}") {
         fun createRoute(groupId: String) = "group_detail/$groupId"
